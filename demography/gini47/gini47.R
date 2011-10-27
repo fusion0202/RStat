8b9d0e62afd92d75c1bef0a9525e576eda0c723c
fusion0202 <sub.master.0202@gmail.com>
2011-10-27 17:25:22 +0900

    1st commit

---
+++ demography/gini47/gini47.R
@@ -0,0 +1,23 @@
+gini.coef <- function(x)
+{
+  if(length(which(is.na(x))) != 0){
+    x <- x[-which(is.na(x))]
+  }
+  n <- length(x)
+  x <- sort(x)
+  x <- cumsum(x)
+  x <- c(0, x/x[n])
+  y <- seq(0, 1, length=n+1)
+  return(2*sum(y-x)/n)
+}
+
+
+pop <- read.csv('pop1920-2010.csv', sep=',', header=T)
+area <- read.csv('area47.csv', sep=',', header=F)
+pop <- pop[-1]
+area <- area[-1]
+area <- rep(area, times=6)
+pop <- pop/area
+res <- apply(pop, 2, gini.coef)
+year <- c(1920, 1945, 1970, 1995, 2005, 2010)
+plot(year, res, ylim=c(0.3,0.7), ylab='Gini Coefficient', las=1, type='b')
