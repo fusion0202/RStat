3797e8fd464249d690d950d548df062a957853b3
fusion0202 <sub.master.0202@gmail.com>
2012-02-09 02:53:07 +0900

    mod

--- https.csv.R
+++ https.csv.R
@@ -1,4 +1,4 @@
-hts.csv <- function(url, header=F){  
+https.csv <- function(url, header=F){  
   require(RCurl)
   text1 <- getURL(url, ssl.verifypeer = FALSE)
   text2 <- unlist(strsplit(text1, "\n"))
