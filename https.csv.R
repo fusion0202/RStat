c00330895ffdcb2ea0c7929c8127470780129bd5
fusion0202 <sub.master.0202@gmail.com>
2012-02-09 01:52:31 +0900

    1st commit

---
+++ https.csv.R
@@ -0,0 +1,19 @@
+https.csv <- function(url, header=F){  
+  require(RCurl)
+  text1 <- getURL(url, ssl.verifypeer = FALSE)
+  text2 <- unlist(strsplit(text1, "\n"))
+  head1 <- unlist(strsplit(text2[1], split="[[:blank:]]"))
+  head2 <- unlist(strsplit(head1, ","))
+  n.col <- length(head2)
+  text3 <- unlist(strsplit(text2, split="[[:blank:]]"))  
+  text4 <- unlist(strsplit(text3, ","))
+  text5 <- text4[text4 != ""]
+  if(header){
+    data <- data.frame(matrix(as.numeric(text5[-(1:n.col)]), ncol=n.col, byrow=T))
+    names(data) <- head2
+  } else {
+    data <- data.frame(matrix(as.numeric(text5), ncol=n.col, byrow=T))
+  }  
+  return(data)
+}
+
